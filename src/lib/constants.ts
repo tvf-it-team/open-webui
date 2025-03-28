dbc63bd5b1c022b036610611e4d7c503be4ab81f
Sam Kandi <samkandi97@gmail.com>
2025-03-28 15:08:20 +0000

    feat: updated name to TVF AI Tools

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 import { browser, dev } from '$app/environment';
 // import { version } from '../../package.json';
 
-export const APP_NAME = 'AI Tools';
+export const APP_NAME = 'TVF AI Tools';
 
 export const WEBUI_HOSTNAME = browser ? (dev ? `${location.hostname}:8080` : ``) : '';
 export const WEBUI_BASE_URL = browser ? (dev ? `http://${WEBUI_HOSTNAME}` : ``) : ``;
